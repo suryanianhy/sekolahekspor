ca4567bf142687387390a4dc63255be15d78b10a
suryanianhy <suryanianhy99@gmail.com>
2022-03-02 20:57:13 +0700

    operator

--- index.php
+++ index.php
@@ -38,12 +38,55 @@
         Kalimat dari Array, Integer dan String: <br>
         <?
             $arr = ['Suca', 28, 'Jakarta'];
-            echo 'Nama saya ' . $arr[0] . ', umur '. $arr[1] . ' tahun. Saya tinggal di ' . $arr[2];
+
+            // pakai petik satu dan operator titik
+            echo 'Nama saya ' . $arr[0] . ', umur '. $arr[1] . ' tahun. Saya tinggal di ' . $arr[2] . '<br>';
+
 
             // pakai petik dua
-
             echo "Nama saya $arr[0], umur $arr[1] tahun. Saya tinggal di $arr[2]";
         ?>
     </h1>
+
+    <h1>
+        Contoh Penggunaan Operator <br>
+        <?
+            $a = 10;
+            $b = 2;
+            $tambah         = $a + $b; 
+            $kurang         = $a - $b; 
+            $kali           = $a * $b; 
+            $bagi           = $a / $b; 
+            $mod            = $a % $b; 
+            $pow            = $a ** $b; 
+            $equal          = $a == $b ? 'True' : 'False'; 
+            $identical      = $a === $b ? 'True' : 'False'; 
+            $not_eq         = $a != $b ? 'True' : 'False'; 
+            $not_identical  = $a != $b ? 'True' : 'False'; 
+            $increment_a    = $a++; 
+            $decrement_a    = $a--; 
+
+            echo "a = $a                        <br>";
+            echo "b = $b                        <br>";
+            echo "a + b = $tambah               <br>";
+            echo "a - b = $kurang               <br>";
+            echo "a x b = $kali                 <br>";
+            echo "a % b = $mod                  <br>";
+            echo "a ** b = $pow                 <br>";
+            echo "a == b = $equal               <br>";
+            echo "a === b = $identical          <br>";
+            echo "a != b = $not_eq              <br>";
+            echo "a !== b = $not_identical      <br>";
+            echo "a++ = $increment_a            <br>";
+            echo "a-- = $increment_a            <br>";
+
+
+        ?>
+    </h1>
+
+
+
+
+    
 </body>
 </html>
